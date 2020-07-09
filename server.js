d391e9d352ff5545d27c9bf0643ebe39e2aa2e07
Jonathas H. Moraes <jonathas.moraes@fatec.sp.gov.br>
2020-07-08 22:33:38 -0300

    Adding CORS code

--- server.js
+++ server.js
@@ -11,8 +11,10 @@ app.use(cors());
 app.options('*', cors()) 
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS,PUT,DELETE');
+
   next();
 });
 
